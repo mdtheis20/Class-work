4e90bb3bbb52dc99b269d6c350856b034dad89d6
Mike Morel <michael.morel@techelevator.com>
2020-05-14 18:52:43 -0400

    M1D4 lecture

---
+++ module-1/04_Loops_Arrays/lecture-final/Arrays/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = 0;
+            i = i + 1;
+            i += 1;
+            i++;
+
+
+
+            //1. Creating an array of integers
+            int[] quizScores = new int[4];
+            quizScores[0] = 100;
+            quizScores[1] = 80;
+            quizScores[2] = 85;
+            quizScores[3] = 90;
+
+            Console.WriteLine($"The length of the array is {quizScores.Length}");
+
+            //2. Creating an array of strings
+            string[] names = new string[] { "Max", "Jason", "Jordann", "Kevin" };
+
+            //3. Create an array of characters that hold "Tech Elevator"        
+            char[] letters = {'M', 'i', 'k', 'e' };
+
+            //4. Print out the first item in each array
+            Console.WriteLine($"First score: {quizScores[0]}");
+            Console.WriteLine($"First name: {names[0]}");
+            Console.WriteLine($"First letter: {letters[0]}");
+
+            string name = "Seth";
+            Console.WriteLine($"First letter of name: {name[0]}");
+
+            //5. Print out the final item in each array
+            Console.WriteLine($"Final element of scores: {quizScores[ quizScores.Length - 1 ]}");
+            Console.WriteLine($"Final name is {names[names.Length - 1]}");
+            //6. Get the length of each array
+
+            //7. Get the last index 
+
+            //8. Update the last item in each array
+            quizScores[quizScores.Length - 1] += 5;
+            names[names.Length - 1] = "Hey his name's !Kevin";
+
+            Console.ReadLine();
+        }
+
+        static public int[] GimmeAnArray(int lengthOfArray)
+        {
+            int[] result = new int[lengthOfArray];
+            return result;
+        }
+    }
+}
---
+++ module-1/04_Loops_Arrays/lecture-final/Lecture/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace Lecture
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1. Use a for-loop to print "Hello World" 10 times
+            for (int i = 1; i <= 10; i++)
+            {
+                Console.WriteLine($"{i}. Hello World");
+            }
+
+            // Print Hello World when is even
+            for (int i = 2; i <= 10; i += 2)
+            {
+                Console.WriteLine($"{i}. Hello World");
+            }
+
+
+
+
+            // 2. Create an array of quiz scores
+            int[] scores = { 90, 100, 56, 78, 92, 67, 89 };
+
+
+            // 3. Print all the scores to the screen
+            Console.WriteLine("The scores are:");
+            for (int i = 0; i < scores.Length; i++)
+            {
+                Console.WriteLine(scores[i]);
+            }
+
+            // 4. Grade on a curve...increase all scores by the curve amount
+            //int theCurve = 4;
+            //Console.WriteLine();
+            //Console.WriteLine("The adjusted scores are:");
+            //for (int i = 0; i < scores.Length; i++)
+            //{
+            //    scores[i] += theCurve;
+            //    //scores[i] = scores[i] + theCurve;
+            //    Console.WriteLine(scores[i]);
+            //}
+
+            // 5. Calculate and print the average score for the class after the curve.
+            int sum = 0;
+            for (int i = 0; i < scores.Length; i++)
+            {
+                sum += scores[i];
+            }
+
+            // Now we have the sum, calculate the average
+            double classAverage = sum / (double)scores.Length;
+
+//            Console.WriteLine($"The class average adjusted is {classAverage}");
+            Console.WriteLine($"The class average adjusted is {sum / (double)scores.Length}");
+
+
+            // Find out if there are any perfect scores
+            bool hasPerfectScores = false;
+            for (int j = 0; j < scores.Length; j++)
+            {
+                if (scores[j] == 100)
+                {
+                    hasPerfectScores = true;
+                    break;
+                }
+            }
+            Console.WriteLine($"Perfect scores: {hasPerfectScores}.");
+
+
+        }
+    }
+}
