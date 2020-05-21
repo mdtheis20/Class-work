031aaecc9221b1f971a5e85bc37f34a827f80189
Mike Morel <michael.morel@techelevator.com>
2020-05-21 12:08:49 -0400

    M1D9 partial lecture code

---
+++ module-1/09_Introduction_Classes/lecture-final/CarDealer/Car.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CarDealer
+{
+    public class Car
+    {
+        public string VIN { get; set; }
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+
+        // Speed is publicly gettable, but not publicly settable (caller must call Accellerate)
+        public int Speed { get; private set; } = 0;
+
+        // Create a derived property.  Age depends on (is derived from) the car's Year property.
+        public int Age
+        {
+            get
+            {
+                int currentYear = DateTime.Now.Year;
+                // Age is the current year minus the Year of the car
+                return currentYear - Year;
+            }
+        }
+
+        // Backed property for Gear
+        private string gear = "P";
+        public string Gear
+        {
+            get
+            {
+                return this.gear;
+            }
+            set
+            {
+                if (value == "P" || value == "D" || value == "N" || value == "R")
+                {
+                    this.gear = value;
+                }
+            }
+        }
+
+        public void Accelerate()
+        {
+            this.Speed++;
+        }
+
+    }
+}
