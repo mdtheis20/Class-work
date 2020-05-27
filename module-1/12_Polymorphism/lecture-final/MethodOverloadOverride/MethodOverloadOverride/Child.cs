f7ae7ba94c84b7cabf8e800ca2bef5478104dbdb
Mike Morel <michael.morel@techelevator.com>
2020-05-27 14:43:00 -0400

    M1D12 lecture

---
+++ module-1/12_Polymorphism/lecture-final/MethodOverloadOverride/MethodOverloadOverride/Child.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MethodOverloadOverride
+{
+    public class Child : Parent
+    {
+        public override int DoMath(int x, int y, int z)
+        {
+            Console.WriteLine(secretCode);
+            return x * y * z;
+        }
+    }
+}
