6d191cfb600928b1ad3b2e27b3cc6c45b03ece81
Mike Morel <michael.morel@techelevator.com>
2020-05-28 14:14:40 -0400

    M1D13 lecture

---
+++ module-1/13_Inheritance_Abstract_Classes/lecture-final/Interfaces/Interfaces/Classes/Person.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Interfaces.Classes
+{
+    /// <summary>
+    /// Represents a person and their vital statistics
+    /// </summary>
+    public class Person : IComparable
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public int HeightInches { get; set; }
+
+        public Person(string first, string last, int age, int height)
+        {
+            FirstName = first;
+            LastName = last;
+            Age = age;
+            HeightInches = height;
+        }
+
+        public override string ToString()
+        {
+            return $"{FirstName} {LastName}: Age {Age}, Height {HeightInches} inches.";
+        }
+
+        public int CompareTo(object obj)
+        {
+            if (!(obj is Person))
+            {
+                return 0;
+            }
+
+            Person other = (Person)obj;
+
+            if (this.HeightInches > other.HeightInches)
+            {
+                return -1;
+            }
+            if (this.HeightInches < other.HeightInches)
+            {
+                return 1;
+            }
+            return 0;
+        }
+    }
+}
---
+++ module-1/13_Inheritance_Abstract_Classes/lecture-final/Shapes/Shapes/Models/Triangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Shapes.Models
+{
+    class Triangle : Shape2D
+    {
+        private int triangleBase;
+        private int height;
+
+        public Triangle(ConsoleColor color, bool isFilled, int triangleBase, int height) : base(color, isFilled)
+        {
+            this.triangleBase = triangleBase;
+            this.height = height;
+        }
+
+        public override int Area
+        {
+            get
+            {
+                return (int)(0.5 * triangleBase * height);
+            }
+        }
+
+        public override int Perimeter
+        {
+            get
+            {
+                return 100;
+            }
+        }
+
+        public override void Draw()
+        {
+            Console.WriteLine("This is a triangle");
+        }
+    }
+}
