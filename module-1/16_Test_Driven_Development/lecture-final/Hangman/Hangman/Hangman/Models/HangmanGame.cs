dba229c0f7d70ce2a3fc3ff068162cfddd59bf35
Mike Morel <michael.morel@techelevator.com>
2020-06-01 14:43:07 -0400

    M1D16 lecture

---
+++ module-1/16_Test_Driven_Development/lecture-final/Hangman/Hangman/Hangman/Models/HangmanGame.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Hangman
+{
+    public class HangmanGame
+    {
+        public HangmanGame(string wordToSolve)
+        {
+            WordToSolve = wordToSolve;
+            Clue = new string('-', wordToSolve.Length);
+            guessHistory = new List<char>();
+        }
+
+        public string WordToSolve { get; }
+        public string Clue { get; private set; }
+
+        private List<char> guessHistory;
+        public char[] GuessHistory {
+            get
+            {
+                return guessHistory.ToArray();
+            }
+        }
+
+        public int Guess(char guess)
+        {
+            // This is the Linq way to get a count of matches
+            //int count = WordToSolve.Count( c => c == guess );
+
+            guessHistory.Add(guess);
+            // Look for the character in the word to solve
+            int count = 0;
+            string newClue = "";
+            for (int i = 0; i < WordToSolve.Length; i++)
+            {
+                if (WordToSolve[i] == guess)
+                {
+                    count++;
+                    newClue += guess;
+                }
+                else
+                {
+                    newClue += Clue[i];
+                }
+            }
+
+            // Update Clue to contain the guessed letters
+            Clue = newClue;
+            return count;
+
+        }
+    }
+}
