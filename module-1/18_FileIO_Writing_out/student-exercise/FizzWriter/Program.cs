706e407a53d0498e9b44f4eee8d84ed1bcf981c1
Matt Theis <mdtheis@gmail.com>
2020-06-04 19:02:16 -0400

    Fizz writer program complete

--- module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
+++ module-1/18_FileIO_Writing_out/student-exercise/FizzWriter/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.IO;
 
 namespace FizzWriter
 {
@@ -6,7 +7,50 @@ namespace FizzWriter
     {
         static void Main(string[] args)
         {
+            string path = Directory.GetCurrentDirectory();
+
+            path = @"..\\..\\..\\..\\FizzBuzz.txt";
+            string FizzBuzz(int v)
+            {
+                string newString = Convert.ToString(v);
+
+
+                if (v % 3 == 0 && v % 5 == 0 || newString.Contains("3") && newString.Contains("5"))
+                {
+                    return "FizzBuzz";
+                }
+
+                if (v % 3 == 0 || newString.Contains("3"))
+                {
+
+
+                    return "Fizz";
+                }
+
+                if (v % 5 == 0 || newString.Contains("5"))
+                {
+                    return "Buzz";
+                }
+
+
+
+                return $"{v}";
+
+            }
+
+            using (StreamWriter sw = new StreamWriter(path, true))
+            {
+                for (int i = 0; i <= 300; i++)
+                {
+                    sw.WriteLine(FizzBuzz(i));
+
+                }
+            }
             Console.WriteLine("Hello World!");
         }
     }
 }
+
+
+
+          
