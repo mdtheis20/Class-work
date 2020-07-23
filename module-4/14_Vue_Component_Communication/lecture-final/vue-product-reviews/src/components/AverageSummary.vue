dbf1bb5084e5c3c5449777d6ef4f5ee06d0c3cf9
Mike Morel <michael.morel@techelevator.com>
2020-07-23 14:14:07 -0400

    M4D14 lecture

---
+++ module-4/14_Vue_Component_Communication/lecture-final/vue-product-reviews/src/components/AverageSummary.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="well" v-on:click="updateFilter" :class="{ selectedRating: $store.state.ratingFilter === 0}">
+    <span class="amount">{{averageRating}}</span>
+    Average Rating
+  </div>
+</template>
+
+<script>
+export default {
+  computed: {
+    averageRating() {
+      if (this.$store.state.reviews.length === 0) {
+        return 0;
+      }
+
+      let sum = this.$store.state.reviews.reduce((accum, review) => {
+        return accum + review.rating;
+      }, 0);
+
+      return (sum / this.$store.state.reviews.length).toFixed(2);
+    },
+  },
+  methods: {
+    updateFilter(){
+      this.$store.commit("UPDATE_FILTER", 0);
+    }
+  }
+};
+</script>
+
+<style>
+div.main div.well-display div.well {
+  display: inline-block;
+  width: 15%;
+  border: 1px black solid;
+  border-radius: 6px;
+  text-align: center;
+  margin: 0.25rem;
+  cursor: pointer;
+}
+
+div.main div.well-display div.well span.amount {
+  color: darkslategray;
+  display: block;
+  font-size: 2.5rem;
+}
+.selectedRating {
+  background-color: lightyellow;
+}
+</style>
