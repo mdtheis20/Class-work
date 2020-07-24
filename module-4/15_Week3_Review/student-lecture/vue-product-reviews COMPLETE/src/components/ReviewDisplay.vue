d7bd9f1cc7dc0ae4c88638b5e158d5fc8930bcf2
Mike Morel <michael.morel@techelevator.com>
2020-07-24 08:57:16 -0400

    M4 Week 3 review student

---
+++ module-4/15_Week3_Review/student-lecture/vue-product-reviews COMPLETE/src/components/ReviewDisplay.vue
@@ -0,0 +1,64 @@
+<template>
+  <div :class="{ 'review': true, 'fab': review.favorite }">
+    <h4>{{review.reviewer}}</h4>
+    <div class="rating">
+      <img src="../assets/star.png" class="ratingStar" v-for="i in review.rating" v-bind:key="i" />
+    </div>
+    <h3>{{review.title}}</h3>
+    <p>{{review.review}}</p>
+    <p>
+      Favorite?
+      <input type="checkbox" :checked="review.favorite" @click="toggleFavorite(review)" />
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+    props: {
+        review: Object
+    },
+    methods: {
+        toggleFavorite(review){
+            this.$store.commit('TOGGLE_FAVORITE', review);
+        }
+    },
+};
+</script>
+
+<style>
+div.review {
+  border: 1px black solid;
+  border-radius: 6px;
+  padding: 1rem;
+  margin: 10px;
+}
+
+div.review div.rating {
+  height: 2rem;
+  display: inline-block;
+  vertical-align: top;
+  margin: 0 0.5rem;
+}
+
+div.review div.rating img {
+  height: 100%;
+}
+
+div.review p {
+  margin: 20px;
+}
+
+div.review h3 {
+  display: inline-block;
+}
+
+div.review h4 {
+  font-size: 1rem;
+}
+
+/* Mark which rating is selected */
+.fab {
+  background-color: lightyellow;
+}
+</style>
---
+++ module-4/15_Week3_Review/student-lecture/vue-product-reviews COMPLETE/src/components/StarSummary.vue
@@ -0,0 +1,58 @@
+<template>
+  <!-- Set the filter when the user clicks on the display -->
+  <!-- Mark which rating is selected by setting the class -->
+
+  <div
+    class="well"
+    @click="updateFilter"
+    :class="{ selectedRating: $store.state.ratingFilter === ratingValue}"
+  >
+    <span class="amount">{{numberReviews}}</span>
+    {{ ratingValue }} Star Review
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+  props: {
+    ratingValue: Number,
+  },
+  computed: {
+    numberReviews() {
+      return this.$store.state.reviews.filter(
+        (r) => r.rating === this.ratingValue
+      ).length;
+    },
+  },
+  methods: {
+    updateFilter() {
+      this.$store.commit("UPDATE_FILTER", this.ratingValue);
+    },
+  },
+};
+</script>
+
+<style>
+.selectedRating {
+  background-color: lightyellow;
+}
+
+div.main div.well-display div.well {
+  display: inline-block;
+  width: 15%;
+  border: 1px black solid;
+  border-radius: 6px;
+  text-align: center;
+  margin: 0.25rem;
+  cursor: pointer;
+}
+
+div.main div.well-display div.well span.amount {
+  color: darkslategray;
+  display: block;
+  font-size: 2.5rem;
+}
+</style>
