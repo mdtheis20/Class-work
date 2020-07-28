826940e59596e845e8d5114f9c741de932d56b07
Mike Morel <michael.morel@techelevator.com>
2020-07-28 08:39:23 -0400

    M4D17 student

---
+++ module-4/17_Vue_Routing/student-lecture/product-reviews/src/views/AddReviewPage.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <h1>Add Review</h1>
+    <add-review :productId="productId"/>
+  </div>
+</template>
+
+<script>
+import AddReview from "@/components/AddReview";
+
+export default {
+  components: {
+    AddReview
+  },
+  data(){
+      return {
+          productId: 0
+      }
+  },
+  created(){
+      this.productId = parseInt(this.$route.params.id)
+  }
+};
+</script>
+
+<style>
+</style>
